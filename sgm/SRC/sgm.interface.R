8d1a8e603c50c87dc9b00e02d6f2c85954d9eab1
Youngser Park <youngser@gmail.com>
2017-09-13 16:28:40 -0400

    modify sgm to meet the graph API

--- sgm/SRC/sgm.interface.R
+++ sgm/SRC/sgm.interface.R
@@ -7,7 +7,7 @@ if(!require(igraph)) {
     library(igraph)
 }
 
-sgm.interface <- function(input1, input2, s)
+sgm.interface <- function(g1, g2, s)
 {
     ## X <- as.matrix(read.table(input1))
     ## if (ncol(X)==2) {
@@ -22,8 +22,8 @@ sgm.interface <- function(input1, input2, s)
     ##     g2 <- graph.adjacency(X)
     ## }
 
-    g1 <- read.graph(input1, "gml")
-    g2 <- read.graph(input2, "gml")
+#    g1 <- read.graph(input1, "gml")
+#    g2 <- read.graph(input2, "gml")
     
     A1 <- as.matrix(g1[]); n <- nrow(A1)
     A2 <- as.matrix(g2[]); m <- nrow(A2)
@@ -35,6 +35,7 @@ sgm.interface <- function(input1, input2, s)
     S[(s+1):n,(s+1):n] <- M
     out <- sgm(A2, A1, 0, start=S, pad=0, iteration=niter)$P
 
-    cat("The output file is saved in '../DATA/out.txt'.\n")
-    write.table(out,"../DATA/out.txt", row.names=F, col.names=F)
+    return(out)
+#    cat("The output file is saved in '../DATA/out.txt'.\n")
+#    write.table(out,"../DATA/out.txt", row.names=F, col.names=F)
 }
