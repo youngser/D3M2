89ca4d613854999a5f496975e589392935880dfe
Disa Mhembere <dmhembe1@jhu.edu>
2017-09-16 07:10:35 -0400

    gclus done

--- gclust/SRC/gclust.interface.R
+++ gclust/SRC/gclust.interface.R
@@ -1,13 +1,8 @@
 if(!require(mclust)){
     install.packages("mclust")
-    library(mclust)
+    suppressMessages(library(mclust))
 }
 
-gclust.interface <- function(input, K=2) {
-    X <- as.matrix(read.table(input))
-    Khat <- Mclust(X, 1:K)$G
-    cat("The optimal number of cluster is ", Khat, "\n")
-
-    cat("The output file is saved in '../DATA/out.txt'.\n")
-    write(Khat,"../DATA/out.txt", ncol=1)
+gclust.interface <- function(X, K=2) {
+    return(Mclust(X, 1:K)$G)
 }
