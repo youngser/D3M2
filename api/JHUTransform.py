af7c0d7365d1a4823545056c8418942fb25d5719
Disa Mhembere <dmhembe1@jhu.edu>
2017-09-17 11:56:21 -0400

    added vnsgm

--- api/JHUTransform.py
+++ api/JHUTransform.py
@@ -4,8 +4,6 @@
 
 import sys, os
 from Transform import Transform
-from JHUGraph import JHUGraph
-from util import gen_graph_r
 import numpy as np
 
 sys.path.append(os.path.abspath("../"))
@@ -20,6 +18,7 @@ from oocase.SRC.oocase import oocase # FIXME: Expects a FlashGraph object
 from nonpar.SRC.nonpar import nonpar
 from sgc.SRC.sgc import sgc
 from numclust.SRC.numclust import numclust
+from vnsgm.SRC.vnsgm import vnsgm
 
 class JHUTransform(Transform):
 
@@ -27,13 +26,13 @@ class JHUTransform(Transform):
         """
         TODO: YP document
         """
-        return np.array(ase(g, dim)[0]) # TODO: YP do we need more?
+        return np.array(ase(g, dim))
 
     def lse_transform(self, g, dim=2):
         """
         TODO: YP document
         """
-        return np.array(ase(g, dim)[0]) # TODO: YP do we need more?
+        return np.array(ase(g, dim))
 
     def ptr_transform(self, g):
         """
@@ -83,54 +82,8 @@ class JHUTransform(Transform):
         """
         return numclust(X)[0]
 
-
-def test():
-    gpath, rig = gen_graph_r(n=50, p=.1)
-
-    g = JHUGraph()
-    g.read_graph(gpath)
-
-    print("Summary: ")
-    g.summary()
-    t = JHUTransform()
-
-    # ASE
-    ASE = t.ase_transform(g, 4)
-    print "ASE: ", ASE, "\n\n"
-
-    # LSE
-    LSE = t.lse_transform(g, 4)
-    print "LSE: ", ASE, "\n\n"
-
-    # PTR
-    PTR = t.ptr_transform(g)
-    print "PTR: ", PTR, "\n\n"
-
-    # DIM
-    DIM = t.dimselect_transform(g.get_adjacency_matrix())
-    print "DIM: ", DIM, "\n\n"
-
-    # SGM
-    gpath, rig2 = gen_graph_r(n=50, p=.1)
-    g2 = JHUGraph()
-    g2.read_graph(gpath)
-
-    SGM = t.sgm_transform(g, g2, 3)
-    print "SGM: ", SGM, "\n\n"
-
-    # GCLUST
-    GCLUST = t.gclust_transform(ASE, 2)
-    print "GCLUST: ", GCLUST, "\n\n"
-
-    # NONPAR
-    # NONPAR = t.nonpar_transform(ASE[:3,:3], LSE[:3, :3])
-    # print "NONPAR: ", NONPAR, "\n\n"
-
-    # SGC
-    SGC = t.sgc_transform(g)
-    print "SGC: ", SGC, "\n\n"
-
-    # NUMCLUST
-    NUMCLUST = t.numclust_transform(SGC)
-    print "NUMCLUST: ", NUMCLUST, "\n\n"
-test()
+    def vnsgm_transform(self, g, g2, voi, numseeds):
+        """
+        TODO: YP document
+        """
+        return np.array(vnsgm(g, g2, voi, numseeds))
