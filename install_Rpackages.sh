3db87a99586686a718c3f0a5d5a24f7fe6c881c3
Youngser Park <youngser@jhu.edu>
2017-09-28 18:14:47 -0400

    add knitr and rmarkdown

--- install_Rpackages.sh
+++ install_Rpackages.sh
@@ -1,3 +1,3 @@
-su - -c "Rscript -e \"install.packages(c('rjson','devtools','Matrix','fpc','mclust','clue','igraph','irlba'), dependencies = c('Depends', 'Imports'), repos='http://cran.us.r-project.org')\""
+su - -c "Rscript -e \"install.packages(c('knitr','rmarkdown','rjson','devtools','Matrix','fpc','mclust','clue','igraph','irlba'), dependencies = c('Depends', 'Imports'), repos='http://cran.us.r-project.org')\""
 
 su - -c "Rscript -e \"devtools::install_github(c('hadley/tidyverse', 'youngser/gmmase', 'youngser/VN'))\""
