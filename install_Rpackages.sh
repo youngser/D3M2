348e57080e971ea8d50ee682e240112480d46e07
Youngser Park <youngser@jhu.edu>
2017-09-28 15:12:06 -0400

    change install_Rpackage.sh to shell script

--- install_Rpackages.sh
+++ install_Rpackages.sh
@@ -1,4 +1,3 @@
-RUN echo "r <- getOption('repos'); r['CRAN'] <- 'http://cran.us.r-project.org'; options(repos = r);" > ~/.Rprofile
-RUN set -ex \
-	&& Rscript -e 'install.packages(c("rjson", "tidyverse", "Matrix", "fpc", "devtools","mclust","clue", "igraph"i, "irlba"), dependencies = c("Depends", "Imports"))' \
-        && Rscript -e 'devtools::install_github(c("youngser/gmmase", "youngser/VN"))' 
+Rscript -e \"install.packages(c('rjson', 'tidyverse','devtools','Matrix','fpc','mclust','clue','igraph','irlba'), dependencies = c('Depends', 'Imports'), repos='http://cran.us.r-project.org')\"
+
+Rscript -e \"devtools::install_github(c('youngser/gmmase', 'youngser/VN'))\"
