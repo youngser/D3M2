b675a6732183d82f3317a5d04807095817d47ae6
Youngser Park <youngser@jhu.edu>
2017-09-28 12:07:30 -0400

    fix install_Rpackages.sh as Dockerfile format

--- install_Rpackages.sh
+++ install_Rpackages.sh
@@ -1,2 +1,4 @@
-sudo su - -c "Rscript -e \"install.packages(c('rjson', 'tidyverse','Matrix','fpc','mclust','clue','igraph','irlba'), dependencies = c('Depends', 'Imports'), repos='http://cran.us.r-project.org')\"" 
-sudo su - -c "Rscript -e \"devtools::install_github(c('youngser/gmmase', 'youngser/VN'))\""
+RUN echo "r <- getOption('repos'); r['CRAN'] <- 'http://cran.us.r-project.org'; options(repos = r);" > ~/.Rprofile
+RUN set -ex \
+	&& Rscript -e 'install.packages(c("rjson", "tidyverse", "Matrix", "fpc", "devtools","mclust","clue", "igraph"i, "irlba"), dependencies = c("Depends", "Imports"))' \
+        && Rscript -e 'devtools::install_github(c("youngser/gmmase", "youngser/VN"))' 
