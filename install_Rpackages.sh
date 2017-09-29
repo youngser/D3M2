152c8c2586e846c22deff8c397ec9bf549f763ed
Youngser Park <youngser@jhu.edu>
2017-09-29 08:28:16 -0400

    add rstudio

--- install_Rpackages.sh
+++ install_Rpackages.sh
@@ -1,3 +1,7 @@
+apt-get install -y gdebi-core
+wget https://download1.rstudio.org/rstudio-1.0.153-amd64.deb
+gdebi --option=APT::Get::force-yes=1,APT::Get::Assume-Yes=1 -n rstudio-1.0.153-amd64.deb
+
 su - -c "Rscript -e \"install.packages(c('knitr','rmarkdown','rjson','devtools','Matrix','fpc','mclust','clue','igraph','irlba'), dependencies = c('Depends', 'Imports'), repos='http://cran.us.r-project.org')\""
 
 su - -c "Rscript -e \"devtools::install_github(c('hadley/tidyverse', 'youngser/gmmase', 'youngser/VN'))\""
